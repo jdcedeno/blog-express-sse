a59ddfe79b1f369fd2cb0ea667e837433fd03085
Jose Cedeno <56703919+jdcedeno@users.noreply.github.com>
2021-04-03 17:02:50 -0400

    completed communication cycle event emitted and received, going to create the query service

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []); // Always send back an array to whoever is making the request
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString("hex"); // ID is pulled from the query string parameter and should belong to a post in the DB of the 'posts' service.
     const { content } = req.body;
 
@@ -23,7 +24,22 @@ app.post("/posts/:id/comments", (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post("http://localhost:4005/events", {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+        },
+    });
+
     res.status(201).send(comments);
 });
 
-app.listen(4001, () => console.log("listening on port 4001"));
+app.post("/events", (req, res) => {
+    console.log("Event Received", req.body.type);
+
+    res.send({});
+});
+
+app.listen(4001, () => console.log("listening on port 4001..."));
